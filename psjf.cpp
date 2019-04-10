98aed95bb13ce84752bdb0c9185efe8cd86bbbc7
adarshbadwaya <46555353+adarshbadwaya@users.noreply.github.com>
2019-04-11 00:00:01 +0530

    Add files via upload

---
+++ psjf.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+      int arrival_time[10], burst_time[10], temp[10];
+      int i, smallest, count = 0, time, limit;
+      double wait_time = 0, turnaround_time = 0, end;
+      float average_waiting_time, average_turnaround_time;
+      cout<<"\nEnter the Total Number of Processes:\t";
+      cin>>limit;
+      cout<<"\nEnter Details of "<<limit<< " Processes\n";
+      for(i = 0; i < limit; i++)
+      {
+            cout<<"Enter Arrival Time of process  "<<i+1<<"    ";
+            cin>>arrival_time[i];
+           cout<<"Enter Burst Time of Process  "<<i+1<<"\t";
+           cin>>burst_time[i]; 
+            temp[i] = burst_time[i];
+      }
+      burst_time[9] = 9999;  
+      for(time = 0; count != limit; time++)
+      {
+            smallest = 9;
+            for(i = 0; i < limit; i++)
+            {
+                  if(arrival_time[i] <= time && burst_time[i] < burst_time[smallest] && burst_time[i] > 0)
+                  {
+                        smallest = i;
+                  }
+            }
+            burst_time[smallest]--;
+            if(burst_time[smallest] == 0)
+            {
+                  count++;
+                  end = time + 1;
+                  wait_time = wait_time + end - arrival_time[smallest] - temp[smallest];
+                  turnaround_time = turnaround_time + end - arrival_time[smallest];
+            }
+      }
+      average_waiting_time = wait_time / limit; 
+      average_turnaround_time = turnaround_time / limit;
+      cout<<"\n\nAverage Waiting Time:\t "<<average_waiting_time<<"\n";
+      cout<<"Average Turnaround Time:\t"<<average_turnaround_time<<"\n";
+      return 0;
+}

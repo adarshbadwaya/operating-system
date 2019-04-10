5be77d0c0ba6de89b3db565e7200b0b6b086a11f
adarshbadwaya <46555353+adarshbadwaya@users.noreply.github.com>
2019-04-11 01:56:54 +0530

    Add files via upload

---
+++ roundrobin.cpp
@@ -0,0 +1,121 @@
+//round robin
+
+#include<iostream>
+
+#define max 100
+
+using namespace std;
+
+class RoundRobin{
+
+	public:
+
+		int n,q,i;
+
+		float bt[max],tat[max],wt[max],rbt[max],t;
+
+		bool done;
+RoundRobin()
+{
+t=0.0;
+done=true;
+}
+		void getdata(){
+
+			cout<<"Enter number of processes = ";
+
+			cin>>n;
+
+			cout<<"Enter Quanta = ";
+
+			cin>>q;
+
+			cout<<"Enter Burst time of processes\n";
+
+			for(i=0;i<n;i++){
+      				cout<<"burst time of process P["<<i+1<<"]";
+				cin>>bt[i];
+
+				rbt[i]=bt[i];
+
+			}
+
+		}
+
+		void cal_time(){
+
+			while(true){
+
+				done=true;
+
+				for(i=0;i<n;i++){
+
+					if(rbt[i]>0){
+
+						done=false;
+
+						if(rbt[i]>q){
+
+							t+=q;
+
+							rbt[i]-=q;
+
+						}
+
+						else{
+
+							t+=rbt[i];
+
+							rbt[i]=0;
+
+							wt[i]=t-bt[i];
+
+							tat[i]=t; //or bt[i]+wt[i];
+
+						}
+
+					}
+
+				}
+
+				
+
+				if(done==true)
+
+				break;
+
+			}	
+
+		}
+
+		void display(){
+
+			cout<<"Turn around time of the processes\n";
+
+			for(i=0;i<n;i++)
+
+				cout<<"Process "<<i+1<<" = "<<tat[i]<<endl;
+
+			cout<<"\nWaiting time of the processes\n";
+
+			for(i=0;i<n;i++)
+
+				cout<<"Process "<<i+1<<" = "<<wt[i]<<endl;	
+
+		}
+
+};
+
+int main(){
+
+	RoundRobin obj;
+
+	obj.getdata();
+
+	obj.cal_time();
+
+	obj.display();
+
+	return 0;
+
+}
